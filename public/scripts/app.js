166d9c57ccb2601bdd442c511bdbec11baafbfa3
Javier Buitrago <jbuitrago2014@fau.edu>
2018-05-20 14:07:04 -0400

    learning JSX structure

---
+++ public/scripts/app.js
@@ -0,0 +1,58 @@
+"use strict";
+
+console.log("App.js is running!");
+
+// JSX - JavaScript XML 
+
+var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Decision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        " This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            " Item one "
+        ),
+        React.createElement(
+            "li",
+            null,
+            " Item two "
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        " Javier Buitrago "
+    ),
+    React.createElement(
+        "p",
+        null,
+        " Age: 28 "
+    ),
+    React.createElement(
+        "p",
+        null,
+        " Location: Miami "
+    )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log("App.js is running!");
+
+// JSX - JavaScript XML 
+
+var template = (
+<div>
+    <h1>Decision App</h1> 
+    <p> This is some info</p>
+    <ol>
+        <li> Item one </li>
+        <li> Item two </li>
+    </ol>
+</div>
+);
+
+var templateTwo = (
+<div>
+    <h1> Javier Buitrago </h1>
+    <p> Age: 28 </p>
+    <p> Location: Miami </p>
+</div>
+);
+
+
+
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
