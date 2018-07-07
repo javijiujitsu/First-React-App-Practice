d0bbe77b1d19610947e03ca44e85dea3323abb9b
Javier Buitrago <jbuitrago2014@fau.edu>
2018-07-07 13:12:19 -0400

    created a subclass

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -10,7 +10,7 @@
          this.name = name; 
          this.age = age; 
      }
-     getGretting() {
+     getGreeting() {
       //   return 'Hi. I am ' + this.name + '!';
       return `Hi. I am ${this.name}!`
      }
@@ -20,10 +20,57 @@
        }
  }
 
- const me = new Person('Javier Buitrago', 28);
+ class Student extends Person  {
+    constructor(name , age , major){
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;    
+    }
+    getDescription(){
+        let description = super.getDescription();
+        if (this.hasMajor()){
+            description += ` their major is ${this.major}.`;
+        }
+        return description;
+    }
+ }
 
- console.log(me.getDescription());
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super (name, age);
+        this.homeLocation = homeLocation;
+    }
+        hasHomelocation() {
+             return !!this.homeLocation;
+        } 
+    
+        getGreeting(){
+        let greeting = super.getGreeting();
+        if (this.hasHomelocation()){
+              greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+         
+}
 
 
- const other = new Person();
- console.log(other.getDescription());
+
+
+// Traveler -> Person
+// add support for homeLocation
+//override getGreeting 
+// 1. Hi. I am Javier Buitrago. I 'm visiting from Miami.
+// 2. Hi. I am Javeir 
+
+
+
+ const me = new Traveler ('Javier Buitrago', 28,  'Miami');
+ console.log(me.getGreeting());
+
+
+ const other = new Traveler(undefined, undefined, "Nowhere");
+ console.log(other.getGreeting());
