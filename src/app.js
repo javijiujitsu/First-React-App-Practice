1cfa990009aaa700fc1f2991192d90ecbf39cde2
Javier Buitrago <jbuitrago2014@fau.edu>
2018-08-11 18:35:46 -0400

    playing around with lifecycle methods

--- src/app.js
+++ src/app.js
@@ -14,6 +14,18 @@ class IndecisionApp extends React.Component {
             options: props.options
         }
     }
+    componentDidMount(){
+        console.log('fetching data');
+    }
+    componentDidUpdate(prevProps, prevState){
+        console.log('saving data');
+    }
+
+    componentWillUnmount(){
+        console.log('componentWillUnmount');
+    }
+
+
     handleDeleteOptions() {
          this.setState(() => ({ options: []  }));
      }
